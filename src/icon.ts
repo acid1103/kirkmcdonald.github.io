aa733c4aab6b9dd7a073bd530007d7dc316128e9
Steven Fontaine <sir.fon1103@gmail.com>
2019-06-21 06:15:00 -0500

    use base64 encoded blank image

--- src/icon.ts
+++ src/icon.ts
@@ -53,7 +53,7 @@ function blankImage() {
     const im = document.createElement("img");
     // Chrome wants the <img> element to have a src attribute, or it will
     // draw a border around it. Cram in this transparent 1x1 pixel image.
-    im.src = "images/pixel.gif";
+    im.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
     return im;
 }
 
